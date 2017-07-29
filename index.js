14aeb92c1a254e07192805bf77875deec402871e
Yana Wilkins <yav9774@gmail.com>
2017-07-29 14:15:37 -0400

    done

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const moment = require('moment')
 const chalk = require('chalk')
 
-//console.log(chalk.blue('Hello world!'));
+console.log(chalk.blue('Hello world!'))
 
 //moment(); from original code
 const now = moment() //.format();//.format came from Cody who got help
 
 //console.log(now) //now I can see today's information
-console.log(`It is ${now.format('LLLL')}`)
-console.log(`It is ${now.dayOfYear()} day of the year`)
-console.log(`It is ${now.format('sssssss')} seconds into the day`)
-//moment({hour: 5, minute: 10, seconds: 20});  // today, 5:10.20.000 WHY ISN'T this working???
+console.log(`It is ${chalk.blue(now.format('LLLL'))}`)
+console.log(`It is ${chalk.red(now.dayOfYear())} day of the year`)
+console.log(`It is ${chalk.blue(now.format('sssssss'))} seconds into the day`)
 
 if (moment().isLeapYear()) {
-  console.log('Gavin, it is a leap Year')
+  console.log('It' + chalk.blue('is') + 'during Daylight Savings Time')
 } else {
-  console.log('it is not')
+  console.log(chalk.red('it is not') + 'a leap year')
 }
